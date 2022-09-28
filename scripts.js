bc0083d83556254a3ab14cbd0f5ca63fe46113d0
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2022-09-27 21:54:33 -0400

    Basic iframe communication

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-console.log("hello");
+const accessExternalIframe = function () {
+  iframeExternal.onload = function () {
+    const iframeWindow = iframeExternal.contentWindow; // Ok
+    try {
+      // const doc = iframeExternal.contentDocument; // Error
+      // const href = iframeExternal.contentWindow.location.href; // Error
+      iframeExternal.contentWindow.location = "/"; // Ok
+      iframeExternal.onload = null;
+    } catch (e) {
+      alert(e);
+    }
+  };
+};
+
+const accessInternalIframe = function () {
+  const oldDoc = iframeInternal.contentDocument;
+  iframeInternal.onload = function () {
+    const newDoc = iframeInternal.contentDocument;
+    console.log(oldDoc === newDoc);
+    // iframeInternal.contentDocument.body.prepend("Hello world");
+  };
+};
+
+accessInternalIframe();
