76c99036f05fcb3f4a870e39b3ed8b69896a1d63
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2022-11-16 21:56:42 -0500

    Add initial examples

--- scripts.js
+++ scripts.js
@@ -1,1 +1,29 @@
-console.log("hello");
+const isEmpty = (obj) => {
+  for (let key in obj) {
+    if (key) return false;
+  }
+
+  return true;
+};
+
+const sumSalaries = (obj) => {
+  let sum = 0;
+
+  for (const key in obj) {
+    sum += obj[key];
+  }
+
+  return sum;
+};
+
+const multiplyNumeric = (obj) => {
+  const multipliedNumeric = { ...obj };
+
+  for (const key in multipliedNumeric) {
+    if (typeof multipliedNumeric[key] === "number") {
+      multipliedNumeric[key] *= 2;
+    }
+  }
+
+  return multipliedNumeric;
+};
